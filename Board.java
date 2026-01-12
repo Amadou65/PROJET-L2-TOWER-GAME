769fbfbeeaaad9df485723729e97a6860e2331bd
Habiba Boubakary <boubakary.habiba.etu@univ-lille.fr>
2026-01-12 17:24:30 +0100

    ajou de classicalBoard

--- Board.java
+++ Board.java
@@ -4,9 +4,9 @@ public class Board{
     private int width;
     private String[][] grid;
     public Board(){
-        this.grid = new int[6][11];
-        for (int i = 0, i < grid.length, i ++){
-            for (int j = 0, j< grid[0].length, j ++){
+        this.grid = new String[6][11];
+        for (int i = 0; i < grid.length; i ++){
+            for (int j = 0; j< grid[0].length; j ++){
                 this.grid[i][j] = cell;
             }
             this.height = 6;
