15ed1b64c65141ce4489031b5652402f3a467b98
Amadou Balde <baldeahmad23@gmail.com>
2026-01-12 20:05:14 +0100

    debut methode path avec obtention de la position de depart

--- game/board/RandomBoard.java
+++ game/board/RandomBoard.java
@@ -1,8 +1,32 @@
+package game.board;
+import java.util.Random;
+import java.util.ArrayList;
 public class RandomBoard extends Board{
     public RandomBoard{
         super();
     }
+    /**
+     * methode that give a random path for the grid
+     * 
+     */
     public void path(){
+        // le chemin doit commencer sur une case du bord au hasard
+        ArrayList<Position> liste_depart = new ArrayList<>();
+        for (int i = 0; i < this.getHeight(); i++){
+            liste_depart.add(new Position(i, 0));
+            liste_depart.add(new Position(i, this.getWidth() - 1));
+            for (int j = 0; j < this.getWidth(); j++){
+                liste_depart.add(new Position(0, j));
+                liste_depart.add(new Position(this.getHeight() - 1, j)); 
+            }
+        }
+        // maintenant qu'on a la liste , on peut choisir une postion au hasard pour debuter
+        //creer un objet random
+        Random randomNumber = new Random();
+        // on va choisir entre 0 et la taile de la lste liste_depart un nombre aleeatoire et on va prendre la position a ce nombre la
+        int size_liste_depart = liste_depart.size();
+        int index_pos = randomNumber.nextInt(size_liste_depart);
+        Position pos_depart = liste_depart.get(index_pos);
         
     }
 }
