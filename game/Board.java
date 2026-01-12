1ea5cdf2e63aff2cb3b29830d33c4ca1ffde0ee9
Amadou Balde <baldeahmad23@gmail.com>
2026-01-12 19:13:55 +0100

    methode display et commentaire

--- game/Board.java
+++ game/Board.java
@@ -1,23 +1,52 @@
 public class Board{
+    // The cell of the Board
     protected Cell cell;
+    // 
     private int height;
     private int width;
-    private String[][] grid;
+    private Cell[][] grid;
     public Board(){
-        this.grid = new String[6][11];
+        this.grid = new Cell[6][11];
         for (int i = 0; i < grid.length; i ++){
             for (int j = 0; j< grid[0].length; j ++){
-                this.grid[i][j] = cell;
+                this.grid[i][j] = new Cell("", new Position(i, j));
+            }
             }
             this.height = 6;
             this.width = 11;
-        }
+        
     }
+    /**
+     * methode that show the grid to the player
+     * @return the grid
+     */
+    public String display(){
+        String s = " 0 1 2 3 4 5 6 7 8 9 10 11\n" ;
+
+        for (int i = 0; i < grid.length; i++){
+            s += " +-+-+-+-+-+-+-+-+-+-+-+-+\n";
+            s += i;
+            for (int j = 0; j < grid[0].length; j++){
+                s += "|" + " ";
+            }
+            s += "|\n";
+        }
+        s += " +-+-+-+-+-+-+-+-+-+-+-+-+";
+        return s;
+    }
+    /**
+     * methode that return the height of the grid
+     * @return the height
+     */
     public int getHeight(){
         return this.height;
     }
+    /**
+     * methode that return the width of the grid
+     * @return the width
+     */
     public int getWidth(){
-        return this.Width;
+        return this.width;
     }
  
 }
