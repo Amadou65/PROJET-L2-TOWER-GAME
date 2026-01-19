bf0aec03b352101268410a69b2dddd6616f0f057
Amadou Balde <baldeahmad23@gmail.com>
2026-01-19 19:36:11 +0100

    Correction methode path

--- src/game/board/RandomBoard.java
+++ src/game/board/RandomBoard.java
@@ -1,5 +1,5 @@
 package game.board;
-
+import game.Board;
 import game.Position;
 import java.util.Random;
 import java.util.ArrayList;
@@ -8,7 +8,10 @@ public class RandomBoard extends Board {
     public RandomBoard() {
         super();
     }
-
+    /**
+     * method that create the list of the starting positions
+     * @return list of a possible start
+     */
     public ArrayList<Position> creerListeDepart() {
         ArrayList<Position> liste_depart = new ArrayList<>();
         for (int i = 0; i < this.getHeight(); i++) {
@@ -25,48 +28,36 @@ public class RandomBoard extends Board {
 
     /**
      * method that gives a random path for the grid
-     * 
+     * @return the list of the path
      */
     public ArrayList<Position> path() {
         // le chemin doit commencer sur une case du board au hasard
-        ArrayList<Position> liste_depart = new ArrayList<>();
-        for (int i = 0; i < this.getHeight(); i++) {
-            liste_depart.add(new Position(i, 0));
-            liste_depart.add(new Position(i, this.getWidth() - 1));
-            
-        }
-        for (int j = 1; j < this.getWidth()-1; j++) {
-            liste_depart.add(new Position(0, j));
-            liste_depart.add(new Position(this.getHeight() - 2, j));
-        }
+        ArrayList<Position> liste_depart = this.creerListeDepart();
         // maintenant qu'on a la liste , on peut choisir une postion au hasard pour
         // debuter
         // creer un objet random
         Random randomNumber = new Random();
         // on va choisir entre 0 et la taile de la lste liste_depart un nombre
         // aleatoire et on va prendre la position a ce nombre la
-        //int size_liste_depart = liste_depart.size();
-        int index_pos = randomNumber.nextInt(liste_depart.size());
-        Position pos_depart = liste_depart.get(index_pos);
+        Position pos_depart = liste_depart.get(randomNumber.nextInt(liste_depart.size()));
         // Maintenant qu'on connait la position de depart on peut commencer le parcours
         // du chemin
         boolean found = false;
         // on va creer la liste du chemin
         ArrayList<Position> path = new ArrayList<>();
-        // on va creer aussi la liste des chemins visitées
-        ArrayList<Position> visited = new ArrayList<>();
         // on ajoute la position de depart a la liste des chemins visités
-        visited.add(pos_depart);
+        path.add(pos_depart);
         while (!found) {
             // si la longueur de la liste visitée n'est pas superieur a 12 et n'est pas sur
-            // un bord
-            if (path.size() < 12) {
+            // un bord ou si la longueur est > et que on est pas sur un bord
+            if (path.size() < 12 || path.size() >= 12 && ! isEdge(path.get(path.size() - 1)) || path.size() >= 12 && isEdge(path.get(path.size() - 1))
+                    && !isSameSide(path.get(path.size() - 1), pos_depart) ) {
                 ArrayList<Position> possible_path = this.nextPositions((pos_depart));
                 Random choice = new Random();
                 int index_aleatoire = choice.nextInt(possible_path.size());
                 // on verifie que la nouvelle case ne fait pas partie d'une case déja visitée
-                for (int i = 0; i < visited.size(); i++) {
-                    if (possible_path.get(index_aleatoire).equals(visited.get(i))) {
+                for (int i = 0; i < path.size(); i++) {
+                    if (possible_path.get(index_aleatoire).equals(path.get(i))) {
                         index_aleatoire = choice.nextInt(possible_path.size());
                     }
 
@@ -74,7 +65,7 @@ public class RandomBoard extends Board {
                 Position case_choisi = possible_path.get(index_aleatoire);
 
                 // on ajoute cette case a liste des cases vsités
-                visited.add(case_choisi);
+                path.add(case_choisi);
             }
             // on va verifier si la longueur de la liste est superieur a 12 qu'on ait sur un
             // bord et pas sur le meme bord de depart
@@ -82,30 +73,16 @@ public class RandomBoard extends Board {
                     && !isSameSide(path.get(path.size() - 1), pos_depart)) {
                 found = true;
             }
-            // Dans ce cas on a une liste > 12 mais on est pas sur un bord ou on est sur le
-            // meme bord de depart
-            else {
-                ArrayList<Position> possible_path = this.nextPositions((pos_depart));
-                Random choice = new Random();
-                int index_aleatoire = choice.nextInt(possible_path.size());
-                // on verifie que la nouvelle case ne fait pas partie d'une case déja visitée
-                for (int i = 0; i < visited.size(); i++) {
-                    if (possible_path.get(index_aleatoire).equals(visited.get(i))) {
-                        index_aleatoire = choice.nextInt(possible_path.size());
-                    }
-
-                }
-                Position case_choisi = possible_path.get(index_aleatoire);
-
-                // on ajoute cette case a liste des cases vsités
-                visited.add(case_choisi);
-            }
 
         }
         // Quand on finit on peur retourner la liste du chemin
         return path;
     }
-
+    /**
+     * 
+     * @param pos
+     * @return
+     */
     /**
      * methode that give the next cell
      * 
