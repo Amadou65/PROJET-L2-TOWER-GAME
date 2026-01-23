c534d93bbae72e0a6167bb45dc22c263c0bde8a9
Serhii IVKIN <serhii.ivkin.etu@univ-lille.fr>
2026-01-23 17:44:27 +0100

    Position Tests

--- src/game/Position.java
+++ src/game/Position.java
@@ -1,4 +1,4 @@
-
+package game;
 public class Position{
     private int x;
     private int y;
@@ -6,13 +6,28 @@ public class Position{
         this.x = x;
         this.y = y;
     }
+    // Display coordinates
     public String display(){
         return "(" + this.x + ", " + this.y + ")";
     }
+
+    // Getters
     public int getX(){
         return this.x;
     }
+
     public int getY(){
         return this.y;
+    }
+
+    // Equality check
+    public boolean equals(Object o){
+        if (o instanceof Position){
+            Position position = (Position) o;
+            return this.x == position.x && this.y == position.y;
+        }
+        else{
+            return false;
+        }
     }
 }
---
+++ tests/game/PositionTest.java
@@ -0,0 +1,31 @@
+package game;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import game.Position;
+
+public class PositionTest {
+
+    @Test
+    public void testPositionConstrucrotAndGet() {
+        Position pos = new Position(2, 3);
+
+        assertEquals(2, pos.getX());
+        assertEquals(3, pos.getY());
+    }
+
+    @Test
+    public void testPositionEquality() {
+        Position pos1 = new Position(2, 3);
+        Position pos2 = new Position(2, 3);
+        Position pos3 = new Position(3, 2);
+
+        assertTrue(pos1.equals(pos2));
+        assertFalse(pos1.equals(pos3));
+    }
+
+    @Test
+    public void testPositionDisplay() {
+        Position pos = new Position(1, 1);
+        assertEquals("(1, 1)", pos.display());
+    }
+}
