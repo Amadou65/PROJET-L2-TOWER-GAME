0859d2ca3c20625b310292d6a1495ea19e29821a
Habiba Boubakary <boubakary.habiba.etu@univ-lille.fr>
2026-01-25 00:02:12 +0100

    modification de balon.java

--- src/game/baloon.java
+++ src/game/baloon.java
@@ -1,20 +1,32 @@
 package game;
-public class Baloon {
-    public int life;
-    public int speed;
-    public int strength;
+public class baloon {
+    private int health;         // Résistance : 1, 2 ou 4 
+    private double speed;       // Vitesse propre 
+    private int pathIndex;      // Position actuelle dans la liste du chemin
+    private boolean frozen;
 
-    public Baloon(int life, int speed, int strength) {
-        this.life = life;
-        this.speed = speed;
-        this.strength = strength;
+    public baloon(int level) {
+        this.health = level;
+        this.speed = 1.0; 
+        this.pathIndex = 0;
+        this.frozen = false;
     }
 
     public void move() {
-        System.out.println("Le ballon se déplace à une vitesse de " + speed);
+        if (!frozen) {
+            this.pathIndex++; 
+        }
     }
 
-    public boolean isSpawn() {
-        return true;
+    public void takeDamage(int damage) {
+        this.health -= damage;
+    }
+
+    public boolean isPopped() {
+        return this.health <= 0;
+    }
+
+    public int getReward() {
+        return 10;
     }
 }
