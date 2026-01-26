e1a2871b61afb55c94bb9a3a91cad62441e90d54
Habiba Boubakary <boubakary.habiba.etu@univ-lille.fr>
2026-01-26 18:17:41 +0100

    main

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,7 +1,11 @@
 package game;
 
 import game.board.RandomBoard;
+import game.board.ClassicalBoard;
+
 
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.println("=== TEST DU LIVRABLE 1 : TOWER DEFENSE ===\n");
+    }
 }
