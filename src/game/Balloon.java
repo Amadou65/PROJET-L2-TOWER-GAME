a6bb5b7205ebe3857da3f53ffbb5b9057fb5f7bd
Balde Amadou <amadou.balde.etu@a10p19.fil.univ-lille.fr>
2026-01-27 17:05:26 +0100

    methode getpathiindex

--- src/game/Balloon.java
+++ src/game/Balloon.java
@@ -1,16 +1,19 @@
 package game;
 
+import java.util.ArrayList;
+
 public class Balloon {
     private int health;         // Résistance : 1, 2 ou 4 
     private double speed;       // Vitesse propre 
     private double distance;    // Distance parcourue
     private boolean frozen;
-
+    private int pathIndex;
     public Balloon(int level) {
         this.health = level;
         this.speed = determineSpeed(level);
         this.distance = 0.0;
         this.frozen = false;
+        this.pathIndex = 0;
     }
 
     private double determineSpeed(int level) {
@@ -20,11 +23,23 @@ public class Balloon {
     }  // Missing closing brace was here
 
     public void move() {
+        // le ballon se deplace d'abord dans la case 
         if (!frozen) {
             this.distance += this.speed;
+            // on regarde si le ballon a traversé une case
+            if (distance >= 1.0){
+                pathIndex ++;
+                distance = 0.0;
+            }
         }
     }
-
+    /**
+     * methode that the give the pathindex
+     * @param damage
+     */
+    public int getPathIndex(){
+        return pathIndex;
+    }
     /**
      * Gestion des dégâts et de la mutation
      * @param damage points de dégâts reçus
@@ -49,4 +64,12 @@ public class Balloon {
     
     // Optional: You might want this method if you need the level/health
     public int getLevel() { return health; }
+
+    /**
+     * methode that say if a balloon hava reached the limit of the board
+     * @param the path
+     */
+    public boolean haveReachedLimit(ArrayList<Position> path){
+
+    }
 }
