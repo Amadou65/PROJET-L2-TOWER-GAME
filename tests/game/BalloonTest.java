d45d786a68f324e8daa792a6e7fca99d072672ae
Habiba Boubakary <boubakary.habiba.etu@univ-lille.fr>
2026-01-29 22:32:52 +0100

    test

---
+++ tests/game/BalloonTest.java
@@ -0,0 +1,15 @@
+package game;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+
+public class BalloonTest {
+
+    @Test
+    public void testBalloonMutationAndHealth() {
+        ArrayList<Position> path = new ArrayList<>();
+        path.add(new Position(0,0));
+        path.add(new Position(0,1));
+    
+}
