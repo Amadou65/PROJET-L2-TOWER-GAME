a21d0abe284ff34cfa10c3d359728b2fd6c03909
Balde Amadou <amadou.balde.etu@a11p14.fil.univ-lille.fr>
2026-02-02 11:22:02 +0100

    sous classes de projectile

---
+++ src/game/projectile/Bomb.java
@@ -0,0 +1,9 @@
+package game.projectile;
+
+
+class Bomb extends Projectile {
+
+    public Bomb() {
+        super(30);
+    }
+}
---
+++ src/game/projectile/Dart.java
@@ -0,0 +1,9 @@
+package game.projectile;
+
+public class Dart {
+
+    public Dart() {
+        super(10);
+    }
+}
+
