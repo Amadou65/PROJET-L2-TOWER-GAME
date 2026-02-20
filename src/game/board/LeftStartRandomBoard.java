3ac3032b74cf55369ab290629900426e739ceb21
Amadou Balde <baldeahmad23@gmail.com>
2026-02-20 16:08:51 +0100

    classe variant de RandomBoard pour le livrable 3 et qui commence par la gauche

---
+++ src/game/board/LeftStartRandomBoard.java
@@ -0,0 +1,37 @@
+package game.board;
+
+import game.*;
+import java.util.*;
+
+/**
+ * A RandomBoard variant where the path always starts from the left border
+ * (column 0).
+ * This is required by Livrable 3a specification.
+ */
+public class LeftStartRandomBoard extends RandomBoard {
+
+    /**
+     * Creates a board where generated paths always start from the left border.
+     * 
+     * @param height number of rows
+     * @param width  number of columns
+     */
+    public LeftStartRandomBoard(int height, int width) {
+        super(height, width);
+    }
+
+    /**
+     * Returns only the left-border cells as potential starting positions.
+     * Overrides the full-perimeter method from RandomBoard.
+     * 
+     * @return list of positions on the left border (column 0)
+     */
+    @Override
+    public ArrayList<Position> creerListeDepart() {
+        ArrayList<Position> leftBorder = new ArrayList<>();
+        for (int i = 0; i < this.getHeight(); i++) {
+            leftBorder.add(new Position(i, 0));
+        }
+        return leftBorder;
+    }
+}
