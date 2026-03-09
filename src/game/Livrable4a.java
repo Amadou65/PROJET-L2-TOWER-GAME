53783e3c857524f28ab4619ea070e9cf1f0053f8
Amadou Balde <baldeahmad23@gmail.com>
2026-03-09 10:53:06 +0100

    Livrable 4a methode main phase 3

--- src/game/Livrable4a.java
+++ src/game/Livrable4a.java
@@ -44,4 +44,9 @@ public class Livrable4a extends Livrable4 {
                 + player.getCredits() + ") ---");
         List<Tower> towers = Livrable4.placeTowers(board, player, height, width);
         System.out.println("Crédits restants après tours : " + player.getCredits());
+                // --- PHASE 3 : Acheter des évolutions sur les tours éligibles ---
+        System.out.println("\n--- ACHAT DES ÉVOLUTIONS ---");
+        Livrable4.buyEvolutions(towers, player);
+        System.out.println("Crédits restants après évolutions : " + player.getCredits());
+
 
