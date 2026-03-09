1ef74aca206dd260d2ba318398f9606779368ae1
Amadou Balde <baldeahmad23@gmail.com>
2026-03-09 10:50:55 +0100

    Livrable 4a methode main

--- src/game/Livrable4a.java
+++ src/game/Livrable4a.java
@@ -3,3 +3,29 @@ package game;
 import java.util.*;
 import game.board.LeftStartRandomBoard;
 
+/**
+ * Livrable4a : scénario A du Livrable 4.
+ * <p>
+ * Crée un plateau aléatoire dont le chemin part obligatoirement du bord gauche,
+ * place des tours via le joueur (avec déduction des crédits), achète des
+ * évolutions sur les tours éligibles, puis lance la manche.
+ * </p>
+ *
+ * <p>
+ * Usage :
+ * </p>
+ * 
+ * <pre>
+ * java -jar livrable4a.jar &lt;hauteur&gt; &lt;largeur&gt; &lt;nbBallons&gt;
+ * </pre>
+ */
+public class Livrable4a extends Livrable4 {
+
+    public static void main(String[] args) {
+        int height = args.length > 0 ? Integer.parseInt(args[0]) : 8;
+        int width = args.length > 1 ? Integer.parseInt(args[1]) : 12;
+        int nbBallons = args.length > 2 ? Integer.parseInt(args[2]) : 5;
+
+        System.out.println("=== LIVRABLE 4A ===");
+        System.out.println("Plateau aléatoire : " + height + "x" + width
+                + " | Ballons : " + nbBallons);
