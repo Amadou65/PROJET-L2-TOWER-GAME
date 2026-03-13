65929b99e3e362bb93beeeb50a64ba9f0da6bb09
Habiba Boubakary <boubakary.habiba.etu@univ-lille.fr>
2026-03-13 15:30:05 +0100

    testplayermoneyforupgrade

--- tests/game/TowerTest.java
+++ tests/game/TowerTest.java
@@ -75,7 +75,7 @@ public class TowerTest {
         t.getEvolution(e);
         assertEquals(2, t.getScope(), "La portée devrait être de 2");
 
-        // Deuxième achat (ne devrait rien cha
+        // Deuxième achat (ne devrait rien
         t.getEvolution(e);
         assertEquals(2, t.getScope(), "La portée ne doit pas augmenter une seconde fois pour la même évolution !");
     }
