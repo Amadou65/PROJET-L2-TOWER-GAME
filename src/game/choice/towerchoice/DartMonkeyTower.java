c3b64351e5adff50996421869fb62a26a40c60d4
Ivkin Serhii <serhii.ivkin.etu@a11p12.fil.univ-lille.fr>
2026-03-16 11:38:06 +0100

    methode getChoice pour towerchoice

--- src/game/choice/towerchoice/DartMonkeyTower.java
+++ src/game/choice/towerchoice/DartMonkeyTower.java
@@ -1,8 +1,15 @@
 package game.choice.towerchoice;
+import game.Tower;
 import game.choice.Choice;
+import game.tower.typeTower.DartMonkey;
 
-public class DartMonkeyTower extends Choice {
+public class DartMonkeyTower extends Choice<Tower> {
+    
     public String toString() {
         return "Dark Monkey";
+    }
+
+    public Tower getChoice() {
+        return new DartMonkey(null, null);
     }
 }
--- src/game/choice/towerchoice/TackShooterTower.java
+++ src/game/choice/towerchoice/TackShooterTower.java
@@ -1,9 +1,15 @@
 package game.choice.towerchoice;
+import game.Tower;
 import game.choice.Choice;
+import game.tower.typeTower.TackShooter;
 
-public class TackShooterTower extends Choice {
+public class TackShooterTower extends Choice<Tower> {
     
     public String toString() {
         return "Tack Shooter";
+    }
+
+    public Tower getChoice() {
+        return new TackShooter(null, null);
     }
 }
