9c0eaabb92948f19ee2e0f8941e7484aaf71574b
Serhii IVKIN <serhii.ivkin.etu@univ-lille.fr>
2026-03-23 10:49:01 +0100

    deplacement de fichier ChoiceTest

--- tests/game/ChoiceTest.java
+++ tests/game/ChoiceTest.java
@@ -1,15 +1,15 @@
+package game;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import game.choice.evolutionchoice.*;
-import game.Evolution;
 
 
 public class ChoiceTest {
     @Test
     public void testEvolutionChoice() {
         Cadence cadenceChoice = new Cadence();
-        
+
         assertEquals(Evolution.EvolutionType.CADENCE, cadenceChoice.getChoice(), "ERREUR : La méthode getChoice() de Cadence ne retourne pas le bon type d'évolution.");
         System.out.println("SUCCÈS : La méthode getChoice() de Cadence retourne le bon type d'évolution.");
     }
