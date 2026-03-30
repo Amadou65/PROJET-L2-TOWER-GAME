e64e6cba0e645e9cb6a48acc973ace8d0eb2adeb
Amadou Balde <baldeahmad23@gmail.com>
2026-03-30 11:13:57 +0200

    methode Evolution

---
+++ src/game/choice/EvolutionChoice.java
@@ -0,0 +1,36 @@
+package game.choice;
+
+import game.Evolution;
+
+/**
+ * Wrapper représentant un choix d'évolution pour le ListChooser.
+ * Affiche le type d'évolution et son coût de manière lisible.
+ */
+public class EvolutionChoice {
+    private Evolution.EvolutionType type;
+    private int cost;
+
+    /**
+     * Crée un choix d'évolution.
+     * 
+     * @param type le type d'évolution
+     * @param cost le coût de l'évolution
+     */
+    public EvolutionChoice(Evolution.EvolutionType type, int cost) {
+        this.type = type;
+        this.cost = cost;
+    }
+
+    public Evolution.EvolutionType getType() {
+        return type;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    @Override
+    public String toString() {
+        return type.name() + " (coût: " + cost + " crédits)";
+    }
+}
