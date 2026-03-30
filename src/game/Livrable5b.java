a9baa5aee34b169d3da13e992c314bfea5b70710
Amadou Balde <baldeahmad23@gmail.com>
2026-03-30 10:31:18 +0200

    Livrable 5b

---
+++ src/game/Livrable5b.java
@@ -0,0 +1,18 @@
+package game;
+
+import java.util.*;
+import game.board.ClassicalBoard;
+import game.listchooser.InteractiveListChooser;
+
+/**
+ * Livrable5b : scénario B du Livrable 5.
+ * <p>
+ * Crée un plateau classique (chemins rectilignes), exécute la phase
+ * d'actions du joueur avec des choix interactifs (InteractiveListChooser),
+ * puis lance la manche.
+ * </p>
+ *
+ * <pre>
+ * java -jar livrable5b.jar &lt;hauteur&gt; &lt;largeur&gt; &lt;nbBallons&gt;
+ * </pre>
+ */
