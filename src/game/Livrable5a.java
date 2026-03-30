68e16ed95072d8833a4d4bc9800fe6c8603143ea
Amadou Balde <baldeahmad23@gmail.com>
2026-03-30 10:28:19 +0200

    livrable 5a

---
+++ src/game/Livrable5a.java
@@ -0,0 +1,18 @@
+package game;
+
+import java.util.*;
+import game.board.LeftStartRandomBoard;
+import game.listchooser.RandomListChooser;
+
+/**
+ * Livrable5a : scénario A du Livrable 5.
+ * <p>
+ * Crée un plateau aléatoire dont le chemin part du bord gauche,
+ * exécute la phase d'actions du joueur avec des choix aléatoires
+ * (RandomListChooser), puis lance la manche.
+ * </p>
+ *
+ * <pre>
+ * java -jar livrable5a.jar &lt;hauteur&gt; &lt;largeur&gt; &lt;nbBallons&gt;
+ * </pre>
+ */
